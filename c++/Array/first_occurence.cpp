01965419fc440e03b0cb527567d79a43b9ddb0e7
lavnesh <lavneshkumar694@gmail.com>
2023-05-19 21:05:59 +0530

    final ans of string

--- c++/Array/first_occurence.cpp
+++ c++/Array/first_occurence.cpp
@@ -1,9 +1,24 @@
 # include<iostream>
 using namespace std;
+ int strStr(string haystack, string needle) {
+        
+        for(int i=0;i<haystack.length();i++){
+        
+                string s= haystack.substr(i,needle.length());
+                // cout<<s<<endl;
+                if(s==needle){
+                    return i;
+                }
+            
+        }
+        return -1;
+        
+    }
 int main(){
     string first ,second;
     cout<<"Enter the parent string "<<endl;
     cin>>first;
     cout<<"Enter the substring to find "<<endl;
     cin>>second;
+    cout<<"The "<<second<<" string is present at index "<<strStr(first,second);
 }
