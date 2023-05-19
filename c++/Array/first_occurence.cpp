1113a48dba56ed7bcd7f5c44193bb8410552d67c
lavnesh <lavneshkumar694@gmail.com>
2023-05-19 21:02:46 +0530

    First occurence of substring in parent string

---
+++ c++/Array/first_occurence.cpp
@@ -0,0 +1,9 @@
+# include<iostream>
+using namespace std;
+int main(){
+    string first ,second;
+    cout<<"Enter the parent string "<<endl;
+    cin>>first;
+    cout<<"Enter the substring to find "<<endl;
+    cin>>second;
+}
