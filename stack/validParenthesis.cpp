099316839e058ef8615fe77ec8c333e8365bd39e
lavnesh <lavneshkumar694@gmail.com>
2023-05-31 22:10:36 +0530

    valid parenthisis check

---
+++ stack/validParenthesis.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+ bool isValid(string s) {
+        stack<char> ans;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='[' || s[i]=='('  || s[i]=='{'){
+                ans.push(s[i]);
+            }
+            else if(!ans.empty()){
+             
+                    if((s[i]==']' && ans.top()=='[') || (s[i]==')' && ans.top()=='(')||(s[i]=='}' && ans.top()=='{')){
+
+                    ans.pop();
+                    }
+                else{
+                    return false;
+                }
+   
+                }
+                else{
+                    return false;
+                }
+            
+        }
+        if(ans.empty()) return true;
+
+        return false;
+}
+int main(){
+    cout<<"Enter the string of brackets";
+    string s ;
+    cin>>s;
+    cout<<isValid(s)?;
+
+}
