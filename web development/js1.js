53451abfe86aa5fdbf029f5f107684f45fe890df
lavnesh <lavneshkumar694@gmail.com>
2023-08-12 09:40:46 +0530

    kuch mt pucho
    again

---
+++ web development/js1.js
@@ -0,0 +1,10 @@
+// java sccript is a dynamically typed language mean a variable is created without a specific type other than c++ and java
+// c++ and java is statically typed language mean a variable is created with a specific data type
+
+// now learn about let and var
+// the scope of let is local means only code blocks but var is created globally means whole code can used it
+// we cannot redeclare let we we can redeclare var
+
+//const is also used locally and initialization is complusory while creating a const but it is not compulsory in case of let and var
+
+// Generaly we declare variable in camel case for ex iLoveYOU means first alphabet of first first word is small and other first alphabet is large
