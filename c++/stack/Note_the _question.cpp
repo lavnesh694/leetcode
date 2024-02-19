95c9b80a900c58742f1df1cc78bdd07fbc41e352
lavnesh <lavneshkumar694@gmail.com>
2024-02-19 21:18:03 +0530

    Update Note_the _question.cpp

--- c++/stack/Note_the _question.cpp
+++ c++/stack/Note_the _question.cpp
@@ -5,7 +5,27 @@
 
 3. Valid parenthesis (most important)(ve can perform this in order of 1 😃 by simply count opening and closing bracket sorry but it is possible if there is only one type of brackets)
 
+4. Minimum add to make parenthesis valid
 
+5. valid parenthesis
+
+6.Background string compare (easy)
+
+7. print Bracket number
+
+8. get minimum at pop
+
+9. Next greater element
+
+10.Next smallest element
+
+11.smallest number on left
+
+12.Next greatest elemnent 2
+
+13.stock span problem(good problem doesn't complete)
+
+so now today iam hereto presemt mu project of thry which we try to cover thjer people who is afraid from the prople who are not alwayus interesgtr so the main thing is not that we are the main peiople who always suppoett to the junction that is niot irganized by tge peoplw whio  white are the maim so hello everyone my name is lavnesh kumar so hello everyone my name is lavnesh kumar and today iam here to tell tyou that iam not afrais from anyoine
 
 
 
